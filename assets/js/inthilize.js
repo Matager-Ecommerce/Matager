34476ac6dcb5eb7c86bcf2ec1d975d9a8c417b40
Ecommerce-ai-5 <omarmohmedamin1000@gmail.com>
2024-08-31 22:17:28 +0300

    Add files via upload

---
+++ assets/js/inthilize.js
@@ -0,0 +1,75 @@
+function _0x27e9(_0x3d1618, _0x3f0cf7) {
+  const _0x3d6cfd = _0x3d6c();
+  return (
+    (_0x27e9 = function (_0x27e93e, _0x1216c0) {
+      _0x27e93e = _0x27e93e - 0x1c2;
+      let _0x47e147 = _0x3d6cfd[_0x27e93e];
+      return _0x47e147;
+    }),
+    _0x27e9(_0x3d1618, _0x3f0cf7)
+  );
+}
+const _0x37e9aa = _0x27e9;
+(function (_0x3d55d8, _0x488012) {
+  const _0x226d2c = _0x27e9,
+    _0x51ed08 = _0x3d55d8();
+  while (!![]) {
+    try {
+      const _0x426671 =
+        (-parseInt(_0x226d2c(0x1c7)) / 0x1) *
+          (parseInt(_0x226d2c(0x1cf)) / 0x2) +
+        -parseInt(_0x226d2c(0x1ca)) / 0x3 +
+        -parseInt(_0x226d2c(0x1d0)) / 0x4 +
+        (parseInt(_0x226d2c(0x1c3)) / 0x5) *
+          (-parseInt(_0x226d2c(0x1c8)) / 0x6) +
+        parseInt(_0x226d2c(0x1cd)) / 0x7 +
+        (parseInt(_0x226d2c(0x1c9)) / 0x8) *
+          (-parseInt(_0x226d2c(0x1d1)) / 0x9) +
+        (-parseInt(_0x226d2c(0x1ce)) / 0xa) *
+          (-parseInt(_0x226d2c(0x1cc)) / 0xb);
+      if (_0x426671 === _0x488012) break;
+      else _0x51ed08["push"](_0x51ed08["shift"]());
+    } catch (_0x1cbb65) {
+      _0x51ed08["push"](_0x51ed08["shift"]());
+    }
+  }
+})(_0x3d6c, 0xa274b);
+const firebaseConfig = {
+  apiKey: _0x37e9aa(0x1cb),
+  authDomain: _0x37e9aa(0x1c2),
+  databaseURL: _0x37e9aa(0x1d3),
+  projectId: _0x37e9aa(0x1c4),
+  storageBucket: _0x37e9aa(0x1d4),
+  messagingSenderId: "922824110897",
+  appId: _0x37e9aa(0x1c6),
+  measurementId: _0x37e9aa(0x1c5),
+};
+function _0x3d6c() {
+  const _0x3a4f09 = [
+    "5hFKSbE",
+    "matager-f1f00",
+    "G-FWS29Z8GMT",
+    "1:922824110897:web:b7978665d22e2d652e7610",
+    "1ngnLyX",
+    "2327622mCyjwo",
+    "72680ydXJOt",
+    "1523178jzdhZK",
+    "AIzaSyDss53pHibCpqo87_1bhoUHkf8Idnj-Fig",
+    "5280rUIcbK",
+    "3678948HVqQpW",
+    "84470yabLUE",
+    "2191506snGfnO",
+    "5003996liTeqC",
+    "666HhQzOT",
+    "initializeApp",
+    "https://matager-f1f00-default-rtdb.firebaseio.com",
+    "matager-f1f00.appspot.com",
+    "matager-f1f00.firebaseapp.com",
+  ];
+  _0x3d6c = function () {
+    return _0x3a4f09;
+  };
+  return _0x3d6c();
+}
+firebase[_0x37e9aa(0x1d2)](firebaseConfig);
+const database = firebase["database"]();
